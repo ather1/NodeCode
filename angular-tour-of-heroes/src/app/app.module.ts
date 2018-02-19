963fe1e1388e3e10d60fcad1a4b8000edf4126e8
ather1 <mohammed_ather@hotmail.com>
2018-02-19 11:54:44 +0000

    herosevice not working

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    // HeroService,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
